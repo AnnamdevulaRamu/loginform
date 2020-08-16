ae331a13e4f4a741a530d0dd85c5e16fa94fc9ab
ASUS <annamdevularamu@gmail.com>
2020-08-16 23:21:23 +0530

    this is my first commit in git

--- app/src/main/java/com/example/loginform/MainActivity.java
+++ app/src/main/java/com/example/loginform/MainActivity.java
@@ -12,39 +12,23 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText editTextemailaddress,editTextpassword;
+    EditText editTextemailaddress, editTextpassword;
     Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextemailaddress=findViewById(R.id.emailaddress);
-        editTextpassword=findViewById(R.id.password);
-        login=findViewById(R.id.login);
+        editTextemailaddress = findViewById(R.id.emailaddress);
+        editTextpassword = findViewById(R.id.password);
+        login = findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String emailaddress = editTextemailaddress.getText().toString();
                 String password = editTextpassword.getText().toString();
-                if (emailaddress.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "please enter valid email id", Toast.LENGTH_SHORT).show();
-                } else if (password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "please enter valid password", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    next();
-            });
-
-        }
-
-            private void next() {
-                mAuth.signInwithEmailandPassword
-            }
-
-            Intent intent=new Intent ( MainActivity.this,welcomepage.class);
-                intent.putExtra( "username", username);
+                Intent intent = new Intent(MainActivity.this, welcomepage.class);
                 startActivity(intent);
             }
         });
